2a25efb7607a925990003e3fd9e8ef7af58ea094
Ali Khalid <ali.kmassri@gmail.com>
2023-02-17 13:48:27 +0200

    first frontend commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 function App() {
+
+  // const [backendData, setBackendData] = useState([])
+
+  // useEffect(() => {
+  //   fetch('/api').then(
+  //     response => response.json().then(
+  //       data => {
+  //         setBackendData(data)
+  //       }
+  //     )
+  //   )
+  // }, [])
+
   return (
-    <div>App</div>
+    <div>
+      {/* {(typeof backendData.users === "undefined") ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => {
+          <p key={i}>{user}</p>
+        })
+      )} */}
+      <p>Users should appear here</p>
+    </div>
   )
 }
 
